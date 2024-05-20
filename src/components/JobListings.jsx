8d12f06adbe22be5ef3e5d2bf9c15d1828db5a8d
kirat14 <moumini.tarik@gmail.com>
2024-05-20 19:12:20 -0400

    Fetch data from server
    Installed json-server to get a full fake REST API using the jobs.json as data source.

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import jobs from "../jobs.json";
 import JobListing from "./JobListing";
 
 const JobListings = ({ isHome = false }) => {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch("http://localhost:3001/jobs");
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log("Failed to fetch data: " + error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchJobs();
+  }, []);
+
   const jobListings = isHome ? jobs.slice(0, 3) : jobs;
   return (
     <section className="bg-blue-50 px-4 py-10">
@@ -11,9 +29,9 @@ const JobListings = ({ isHome = false }) => {
           Browse Jobs
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobListings.map((job) => (
-            <JobListing key={job.id} job={job} />
-          ))}
+          {loading
+            ? "Loading..."
+            : jobListings.map((job) => <JobListing key={job.id} job={job} />)}
         </div>
       </div>
     </section>
