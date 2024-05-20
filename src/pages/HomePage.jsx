7012aa11852b8624c73afb7d673ae41fcb26720d
kirat14 <moumini.tarik@gmail.com>
2024-05-20 11:32:07 -0400

    Create a main layout and a HomePage.

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Hero from "../components/Hero";
+import HomeCards from "../components/HomeCards";
+import JobListings from "../components/JobListings";
+import ViewAll from "../components/ViewAll";
+
+const HomePage = () => {
+  return (
+    <>
+      <Hero
+        title="Become a React Dev"
+        subtitle="Find the React job that fits your skills and needs"
+      />
+      <HomeCards />
+      <JobListings />
+      <ViewAll />
+
+      <script src="js/main.js"></script>
+    </>
+  );
+};
+
+export default HomePage;
