c2da90c3ca30aac018ca11051f32d91aaee48755
kirat14 <moumini.tarik@gmail.com>
2024-05-20 19:27:52 -0400

    Add spinner while fetching data

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from "react";
-import jobs from "../jobs.json";
+import ClipLoader from "react-spinners/ClipLoader";
+
 import JobListing from "./JobListing";
 
 const JobListings = ({ isHome = false }) => {
   const [jobs, setJobs] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const overrideSpinner = {
+    display: "block",
+    margin: "100px auto",
+  };
 
   useEffect(() => {
     const fetchJobs = async () => {
@@ -28,11 +34,21 @@ const JobListings = ({ isHome = false }) => {
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           Browse Jobs
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {loading
-            ? "Loading..."
-            : jobListings.map((job) => <JobListing key={job.id} job={job} />)}
-        </div>
+
+        {loading ? (
+          <ClipLoader
+            color="#4338ca"
+            loading={loading}
+            cssOverride={overrideSpinner}
+            size={150}
+          />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobListings.map((job) => (
+              <JobListing key={job.id} job={job} />
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
